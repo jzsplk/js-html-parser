f44163bb51bdc74596b90f025a3f3a77082e2f92
Alex <jzsplking@gmail.com>
2021-05-04 22:44:37 +0800

    ✅ add test case for tokenzier

--- packages/html-tokenizer/__tests__/html-tokenizer.test.ts
+++ packages/html-tokenizer/__tests__/html-tokenizer.test.ts
@@ -1,5 +1,73 @@
-import {htmlTokenizer} from '../lib/html-tokenizer'
+import {Tokenizer} from '../lib/html-tokenizer'
 
 describe('html-tokenizer', () => {
-    it.todo('needs tests');
+    it('should return right open tag and end tag and char', () => {
+        const tokenizer = new Tokenizer();
+        
+        const demoHtml = '<html><div>mock content</div></html>'
+        
+        expect([...tokenizer._tokenize(demoHtml)]).toEqual([
+                    {
+                     "attrs": [],
+                     "kind": "StartTag",
+                     "name": "html",
+                     "self_closing": false,
+                   },
+                    {
+                     "attrs":  [],
+                     "kind": "StartTag",
+                     "name": "div",
+                     "self_closing": false,
+                   },
+                    {
+                     "content": "m",
+                   },
+                    {
+                     "content": "o",
+                   },
+                    {
+                     "content": "c",
+                   },
+                    {
+                     "content": "k",
+                   },
+                    {
+                     "content": " ",
+                   },
+                    {
+                     "content": "c",
+                   },
+                    {
+                     "content": "o",
+                   },
+                    {
+                     "content": "n",
+                   },
+                    {
+                     "content": "t",
+                   },
+                    {
+                     "content": "e",
+                   },
+                    {
+                     "content": "n",
+                   },
+                    {
+                     "content": "t",
+                   },
+                    {
+                     "attrs":  [],
+                     "kind": "EndTag",
+                     "name": "div",
+                     "self_closing": false,
+                   },
+                    {
+                     "attrs": [],
+                     "kind": "EndTag",
+                     "name": "html",
+                     "self_closing": false,
+                   },
+                 
+        ])
+    });
 });
