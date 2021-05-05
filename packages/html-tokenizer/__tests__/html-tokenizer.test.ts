5b4bd5ba4c155bc59a147c81bc5c6a6809f45c71
Alex <jzsplking@gmail.com>
2021-05-05 08:05:49 +0800

    ✅ add test case

--- packages/html-tokenizer/__tests__/html-tokenizer.test.ts
+++ packages/html-tokenizer/__tests__/html-tokenizer.test.ts
@@ -1,45 +1,83 @@
-import {Tokenizer} from '../lib/html-tokenizer'
+import { Tokenizer } from "../lib/html-tokenizer";
 
-describe('html-tokenizer', () => {
-    it('should return right open tag and end tag and char', () => {
-        const tokenizer = new Tokenizer();
-        
-        const demoHtml = '<html><div>mock content</div></html>'
-        
-        expect([...tokenizer.tokenize(demoHtml)]).toEqual([
-                    {
-                     "attrs": [],
-                     "kind": "StartTag",
-                     "name": "html",
-                     "self_closing": false,
-                     "type": "TagToken",
-                   },
-                    {
-                     "attrs":  [],
-                     "kind": "StartTag",
-                     "name": "div",
-                     "self_closing": false,
-                     "type": "TagToken",
-                   },
-                    {
-                     "content": "mock content",
-                     "type": "CharacterToken",
-                   },
-                    {
-                     "attrs":  [],
-                     "kind": "EndTag",
-                     "name": "div",
-                     "self_closing": false,
-                     "type": "TagToken",
-                   },
-                    {
-                     "attrs": [],
-                     "kind": "EndTag",
-                     "name": "html",
-                     "self_closing": false,
-                     "type": "TagToken",
-                   },
-                 
-        ])
-    });
+describe("html-tokenizer", () => {
+  it("should return right open tag and end tag and char", () => {
+    const tokenizer = new Tokenizer();
+
+    const demoHtml = "<html><div>mock content</div></html>";
+
+    expect([...tokenizer.tokenize(demoHtml)]).toEqual([
+      {
+        attrs: [],
+        kind: "StartTag",
+        name: "html",
+        self_closing: false,
+        type: "TagToken",
+      },
+      {
+        attrs: [],
+        kind: "StartTag",
+        name: "div",
+        self_closing: false,
+        type: "TagToken",
+      },
+      {
+        content: "mock content",
+        type: "CharacterToken",
+      },
+      {
+        attrs: [],
+        kind: "EndTag",
+        name: "div",
+        self_closing: false,
+        type: "TagToken",
+      },
+      {
+        attrs: [],
+        kind: "EndTag",
+        name: "html",
+        self_closing: false,
+        type: "TagToken",
+      },
+    ]);
+  });
+
+  it("should return right token with tokenize method", () => {
+    const demoHtml = "<html><div>mock content</div></html>";
+
+    expect(Tokenizer.tokenize(demoHtml)).toEqual([
+      {
+        attrs: [],
+        kind: "StartTag",
+        name: "html",
+        self_closing: false,
+        type: "TagToken",
+      },
+      {
+        attrs: [],
+        kind: "StartTag",
+        name: "div",
+        self_closing: false,
+        type: "TagToken",
+      },
+      {
+        content: "mock content",
+        type: "CharacterToken",
+      },
+      {
+        attrs: [],
+        kind: "EndTag",
+        name: "div",
+        self_closing: false,
+        type: "TagToken",
+      },
+      {
+        attrs: [],
+        kind: "EndTag",
+        name: "html",
+        self_closing: false,
+        type: "TagToken",
+      },
+    ]);
+  });
 });
