ec459dfaaa99d49d60390653c45b79b0971177b7
Alex <jzsplking@gmail.com>
2021-05-16 11:12:51 +0800

    ✨ add fromCharSet util

--- packages/html-tokenizer/lib/utils.ts
+++ packages/html-tokenizer/lib/utils.ts
@@ -1,3 +1,10 @@
 export const isAsciiLetter = (str: string):boolean => {
     return str.length === 1 && Boolean(/[a-zA-Z]/i.exec(str));
 } 
+
+export const fromCharSet = (charCodes: number[]): Set<string> => {
+    const numberSet = new Set(charCodes);
+    const charSet = new Set([...numberSet].map(code => String.fromCharCode(code)))
+    
+    return charSet;
+}
