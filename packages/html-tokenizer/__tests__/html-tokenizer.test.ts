aea73a154b7f3e3c16a164e519e054f304de68e2
jzsplk <jzsplking@gmail.com>
2021-05-22 14:58:32 +0800

    ✅ update test result

--- packages/html-tokenizer/__tests__/html-tokenizer.test.ts
+++ packages/html-tokenizer/__tests__/html-tokenizer.test.ts
@@ -2,82 +2,158 @@ import { Tokenizer } from "../lib/html-tokenizer";
 
 describe("html-tokenizer", () => {
   it("should return right open tag and end tag and char", () => {
-    const tokenizer = Tokenizer.from({});
-
     const demoHtml = "<html><div>mock content</div></html>";
+    const tokenizer = Tokenizer.from(demoHtml, {});
+    expect([...tokenizer.tokenize(demoHtml)]).toMatchInlineSnapshot(`
+      Array [
+        HtmlTagToken {
+          "attributes": Array [],
+          "begin": 0,
+          "end": 6,
+          "file": undefined,
+          "input": "<html><div>mock content</div></html>",
+          "kind": "HtmlTagToken",
+          "name": "html",
+          "self_closing": false,
+          "type": "StartTag",
+        },
+        HtmlTagToken {
+          "attributes": Array [],
+          "begin": 6,
+          "end": 11,
+          "file": undefined,
+          "input": "<html><div>mock content</div></html>",
+          "kind": "HtmlTagToken",
+          "name": "div",
+          "self_closing": false,
+          "type": "StartTag",
+        },
+        CharacterToken {
+          "begin": 11,
+          "data": "mock content",
+          "end": 23,
+          "file": undefined,
+          "input": "<html><div>mock content</div></html>",
+          "kind": "CharacterToken",
+        },
+        HtmlTagToken {
+          "attributes": Array [],
+          "begin": 23,
+          "end": 29,
+          "file": undefined,
+          "input": "<html><div>mock content</div></html>",
+          "kind": "HtmlTagToken",
+          "name": "div",
+          "self_closing": false,
+          "type": "EndTag",
+        },
+        HtmlTagToken {
+          "attributes": Array [],
+          "begin": 29,
+          "end": 36,
+          "file": undefined,
+          "input": "<html><div>mock content</div></html>",
+          "kind": "HtmlTagToken",
+          "name": "html",
+          "self_closing": false,
+          "type": "EndTag",
+        },
+      ]
+    `);
 
-    expect([...tokenizer.tokenize(demoHtml)]).toEqual([
-      {
-        attrs: [],
-        kind: "StartTag",
-        name: "html",
-        self_closing: false,
-        type: "TagToken",
-      },
-      {
-        attrs: [],
-        kind: "StartTag",
-        name: "div",
-        self_closing: false,
-        type: "TagToken",
-      },
-      {
-        data: "mock content",
-        type: "CharacterToken",
-      },
-      {
-        attrs: [],
-        kind: "EndTag",
-        name: "div",
-        self_closing: false,
-        type: "TagToken",
-      },
-      {
-        attrs: [],
-        kind: "EndTag",
-        name: "html",
-        self_closing: false,
-        type: "TagToken",
-      },
-    ]);
+    // expect([...tokenizer.tokenize(demoHtml)]).toEqual([
+    //   {
+    //     attrs: [],
+    //     kind: "StartTag",
+    //     name: "html",
+    //     self_closing: false,
+    //     type: "TagToken",
+    //   },
+    //   {
+    //     attrs: [],
+    //     kind: "StartTag",
+    //     name: "div",
+    //     self_closing: false,
+    //     type: "TagToken",
+    //   },
+    //   {
+    //     data: "mock content",
+    //     type: "CharacterToken",
+    //   },
+    //   {
+    //     attrs: [],
+    //     kind: "EndTag",
+    //     name: "div",
+    //     self_closing: false,
+    //     type: "TagToken",
+    //   },
+    //   {
+    //     attrs: [],
+    //     kind: "EndTag",
+    //     name: "html",
+    //     self_closing: false,
+    //     type: "TagToken",
+    //   },
+    // ]);
   });
 
   it("should return right token with tokenize method", () => {
     const demoHtml = "<html><div>mock content</div></html>";
-
-    expect([...Tokenizer.tokenize(demoHtml)]).toEqual([
-      {
-        attrs: [],
-        kind: "StartTag",
-        name: "html",
-        self_closing: false,
-        type: "TagToken",
-      },
-      {
-        attrs: [],
-        kind: "StartTag",
-        name: "div",
-        self_closing: false,
-        type: "TagToken",
-      },
-      {
-        data: "mock content",
-        type: "CharacterToken",
-      },
-      {
-        attrs: [],
-        kind: "EndTag",
-        name: "div",
-        self_closing: false,
-        type: "TagToken",
-      },
-      {
-        attrs: [],
-        kind: "EndTag",
-        name: "html",
-        self_closing: false,
-        type: "TagToken",
-      },
-    ]);
+    expect([...Tokenizer.tokenize(demoHtml)]).toMatchInlineSnapshot(`
+      Array [
+        HtmlTagToken {
+          "attributes": Array [],
+          "begin": 0,
+          "end": 6,
+          "file": undefined,
+          "input": "<html><div>mock content</div></html>",
+          "kind": "HtmlTagToken",
+          "name": "html",
+          "self_closing": false,
+          "type": "StartTag",
+        },
+        HtmlTagToken {
+          "attributes": Array [],
+          "begin": 6,
+          "end": 11,
+          "file": undefined,
+          "input": "<html><div>mock content</div></html>",
+          "kind": "HtmlTagToken",
+          "name": "div",
+          "self_closing": false,
+          "type": "StartTag",
+        },
+        CharacterToken {
+          "begin": 11,
+          "data": "mock content",
+          "end": 23,
+          "file": undefined,
+          "input": "<html><div>mock content</div></html>",
+          "kind": "CharacterToken",
+        },
+        HtmlTagToken {
+          "attributes": Array [],
+          "begin": 23,
+          "end": 29,
+          "file": undefined,
+          "input": "<html><div>mock content</div></html>",
+          "kind": "HtmlTagToken",
+          "name": "div",
+          "self_closing": false,
+          "type": "EndTag",
+        },
+        HtmlTagToken {
+          "attributes": Array [],
+          "begin": 29,
+          "end": 36,
+          "file": undefined,
+          "input": "<html><div>mock content</div></html>",
+          "kind": "HtmlTagToken",
+          "name": "html",
+          "self_closing": false,
+          "type": "EndTag",
+        },
+      ]
+    `);
   });
 });
