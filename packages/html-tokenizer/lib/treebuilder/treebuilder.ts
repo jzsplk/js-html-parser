c02bbc9b1e37e448e196fc487731634a8b458a5b
jzsplk <jzsplking@gmail.com>
2021-05-23 20:52:08 +0800

    🚧 add logic for step in tree builder

--- packages/html-tokenizer/lib/treebuilder/treebuilder.ts
+++ packages/html-tokenizer/lib/treebuilder/treebuilder.ts
@@ -1,7 +1,14 @@
 import { TopLevelToken } from "../tokens/tokens";
-import { TreeBuildState } from "./state";
-import { TokenSinkResult,TokenKind, TokenSink } from "../types";
-import {ProcessResult  } from "./types"; 
+import { TreeBuildMode } from "./state";
+import { TokenSinkResult, TokenKind, TokenSink } from "../types";
+import { ProcessResult } from "./types";
+import { fromCharSet, isCharacterToken,isHtmlTagToken,matchTagToken } from "../utils";
+
+interface ProcessData {
+  processResult: ProcessResult;
+  mode: TreeBuildMode;
+  token: TopLevelToken;
+}
 
 export class TreeBuilder implements TokenSink {
   sink: any;
@@ -9,63 +16,96 @@ export class TreeBuilder implements TokenSink {
   // Tokens
   tokens: TopLevelToken[];
   /// Insertion mode.
-  mode: TreeBuildState;
+  mode: TreeBuildMode;
 
-  orig_mode: TreeBuildState | null;
+  orig_mode: TreeBuildMode | null;
   /// Stack of open elements, most recently added at end.
   open_elems: any[];
 
   constructor({ sink, tokens }: { sink: any; tokens: TopLevelToken[] }) {
     this.sink = sink;
-    this.mode = TreeBuildState.Initial;
+    this.mode = TreeBuildMode.Initial;
     this.open_elems = [];
     this.tokens = tokens;
     this.orig_mode = null;
   }
-  
-  // TODO
-  insert_element() {
-      
-  }
-  
-  // TODO
-  insert_element_for() {
 
-  }        
-  
-  reprocess(mode: TreeBuildState, token: TopLevelToken) {
-    this.mode = mode;
+  // TODO
+  insert_element() {}
 
-  }
+  // TODO
+  insert_element_for() {}
+
+  // reprocess(mode: TreeBuildState, token: TopLevelToken) {
+  //   this.mode = mode;
+  // }
 
   process_token(token: TopLevelToken): TokenSinkResult {
     return this.process_token_to_complete(token);
   }
 
   end() {}
-  
+
   process_token_to_complete(token: TopLevelToken): TokenSinkResult {
-      // while(true) {
-    let res = this.setp(this.mode, token);        
-      // }
+    let res: ProcessData | null;
+    while (true) {
+      res = this.setp(this.mode, token);
+      // console.log('process res', res);
+      if (res.processResult === ProcessResult.Done) {
+        break;
+      } else if (res.processResult === ProcessResult.Reprocess) {
+        console.log("reprocess with token:", token.getText());
+        this.mode = res.mode;
+        token = res.token;
+      }
+    }
     return TokenSinkResult.Continue;
   }
-  
-  setp(mode: TreeBuildState, token: TopLevelToken): ProcessResult {
-      switch (mode) {
-          case TreeBuildState.Initial:
-              console.log('step token', token);
-             if(token.kind === TokenKind.CharacterToken) {
-                // TODO: do this logic later
-             } else {
-                this.reprocess(TreeBuildState.BeforeHtml, token);
-             } 
-            break;
-      
-          default:
-              console.log('step token', token);
-              break;
-      }
-      return ProcessResult.Done;
+
+  setp(mode: TreeBuildMode, token: TopLevelToken): ProcessData {
+    switch (mode) {
+      case TreeBuildMode.Initial:
+        console.log("step token in initial", token);
+        if (isCharacterToken(token)) {
+          // TODO: do this logic later
+          return { processResult: ProcessResult.Done, mode: this.mode, token };
+        } else {
+          return {
+            processResult: ProcessResult.Reprocess,
+            token,
+            mode: TreeBuildMode.BeforeHtml,
+          };
+        }
+        break;
+
+      case TreeBuildMode.BeforeHtml:
+        // A character token that is one of U+0009 CHARACTER TABULATION, U+000A LINE FEED (LF), U+000C FORM FEED (FF), U+000D CARRIAGE RETURN (CR), or U+0020 SPACE
+        // Ignore the token
+        if (
+          isCharacterToken(token) &&
+          [...fromCharSet([0x0009, 0x000a, 0x000c, 0x000d, 0x0020])].includes(
+            token.data
+          )
+        ) {
+          // ignore
+          return {processResult: ProcessResult.Done, token, mode: this.mode};
+        } else if(matchTagToken(token, ['<html>'])) {
+          console.log('match <html>', mode, token.getText());
+          this.mode = TreeBuildMode.BeforeHead;
+          return {processResult: ProcessResult.Done, token, mode: this.mode};
+        } else if(matchTagToken(token, ['</head>', '</body>', '</html>', '</br>'])) {
+          console.log('match tag', mode, token.getText());
+          return { processResult: ProcessResult.Done, token, mode: this.mode };
+        } else {
+          console.log('else', token.getText());
+          return { processResult: ProcessResult.Done, token, mode: this.mode };
+        }
+        break;
+
+      default:
+        console.log('step in default', this.mode);
+        return { processResult: ProcessResult.Done, token, mode: this.mode };
+        break;
+    }
   }
 }
