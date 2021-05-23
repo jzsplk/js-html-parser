34096e0a9fc0e0f9d868e7a74bd7e97d82fd8f5f
jzsplk <jzsplking@gmail.com>
2021-05-23 20:49:07 +0800

    ✨ add util for match token and type guard

--- packages/html-tokenizer/lib/utils.ts
+++ packages/html-tokenizer/lib/utils.ts
@@ -1,17 +1,43 @@
-import util from 'util';
+import util from "util";
+import { TokenKind } from "./types";
+import { TopLevelToken, CharacterToken, HtmlTagToken } from "./tokens/tokens";
 
-export const isAsciiLetter = (str: string):boolean => {
-    return str.length === 1 && Boolean(/[a-zA-Z]/i.exec(str));
-} 
+export const isAsciiLetter = (str: string): boolean => {
+  return str.length === 1 && Boolean(/[a-zA-Z]/i.exec(str));
+};
 
 export const fromCharSet = (charCodes: number[]): Set<string> => {
-    const numberSet = new Set(charCodes);
-    const charSet = new Set([...numberSet].map(code => String.fromCharCode(code)))
-    
-    return charSet;
-}
+  const numberSet = new Set(charCodes);
+  const charSet = new Set(
+    [...numberSet].map((code) => String.fromCharCode(code))
+  );
+
+  return charSet;
+};
 
 export const logDeep = (target: any) => {
-    console.log(util.inspect(target, false, null, true));
+  console.log(util.inspect(target, false, null, true));
+};
+
+export const isCharacterToken = (
+  token: TopLevelToken
+): token is CharacterToken => {
+  return "data" in token && token.kind === TokenKind.CharacterToken;
+};
+
+export const isHtmlTagToken = (token: TopLevelToken): token is HtmlTagToken => {
+  return token.kind === TokenKind.HtmlTagToken;
+};
+
+export const matchTagToken = (token: TopLevelToken, tags: string[]): boolean => {
+  if(!isHtmlTagToken(token)) {
+    return false;
+  }
+  if(tags.join('').includes('/')) {
+    return tags.includes(`</${token.name}>`);
+  } else if(tags.join('').includes('<')) {
+    return tags.includes(`<${token.name}>`)
+  }
+
+  return false;
 }
-  
